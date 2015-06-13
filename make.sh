314dc6a476139cf66cce880d627a67161c669305
Geno1024 <754097987@qq.com>
2015-06-13 10:25:49 +0800

    [debug] use sh to avoid ANSI in MS-DOS like prompt

---
+++ make.sh
@@ -0,0 +1,22 @@
+basepath=$(cd `dirname $0`; pwd)
+cd $basepath
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯＋_－\)╯　┴——┴　　　　　　　　　╰\(＋_－╰\).html\"\> > \(╯－_－\)╯┬——┬　　　　　　　　　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　┬——┬　　　　　　　　╰\(－_－╰\).html\"\> > \(╯＋_－\)╯　┴——┴　　　　　　　　　╰\(＋_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_＋\)╯　　　┴——┴　　　　　　　╰\(－_＋╰\).html\"\> > \(╯－_－\)╯　　┬——┬　　　　　　　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　┬——┬　　　　　　╰\(－_－╰\)_.html\"\> > \(╯－_＋\)╯　　　┴——┴　　　　　　　╰\(－_＋╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯＋_－\)╯　　　　　┴——┴　　　　　╰\(＋_－╰\).html\"\> > \(╯－_－\)╯　　　　┬——┬　　　　　　╰\(－_－╰\)_.html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　　　┬——┬　　　　╰\(－_－╰\)_.html\"\> > \(╯＋_－\)╯　　　　　┴——┴　　　　　╰\(＋_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_＋\)╯　　　　　　　┴——┴　　　╰\(－_＋╰\).html\"\> > \(╯－_－\)╯　　　　　　┬——┬　　　　╰\(－_－╰\)_.html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　　　　　┬——┬　　╰\(－_－╰\)_.html\"\> > \(╯－_＋\)╯　　　　　　　┴——┴　　　╰\(－_＋╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯＋_－\)╯　　　　　　　　　┴——┴　╰\(＋_－╰\).html\"\> > \(╯－_－\)╯　　　　　　　　┬——┬　　╰\(－_－╰\)_.html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　　　　　　　┬——┬╰\(－_－╰\).html\"\> > \(╯＋_－\)╯　　　　　　　　　┴——┴　╰\(＋_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_＋\)╯　　　　　　　　　┴——┴　╰\(－_＋╰\).html\"\> > \(╯－_－\)╯　　　　　　　　　　┬——┬╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　　　　　┬——┬　　╰\(－_－╰\).html\"\> > \(╯－_＋\)╯　　　　　　　　　┴——┴　╰\(－_＋╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯＋_－\)╯　　　　　　　┴——┴　　　╰\(＋_－╰\).html\"\> > \(╯－_－\)╯　　　　　　　　┬——┬　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　　　┬——┬　　　　╰\(－_－╰\).html\"\> > \(╯＋_－\)╯　　　　　　　┴——┴　　　╰\(＋_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_＋\)╯　　　　　┴——┴　　　　　╰\(－_＋╰\).html\"\> > \(╯－_－\)╯　　　　　　┬——┬　　　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　　　　┬——┬　　　　　　╰\(－_－╰\).html\"\> > \(╯－_＋\)╯　　　　　┴——┴　　　　　╰\(－_＋╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯＋_－\)╯　　　┴——┴　　　　　　　╰\(＋_－╰\).html\"\> > \(╯－_－\)╯　　　　┬——┬　　　　　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯　┬——┬　　　　　　　　　╰\(－_－╰\).html\"\> > \(╯＋_－\)╯　　　┴——┴　　　　　　　╰\(＋_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_＋\)╯┴——┴　　　　　　　　　　╰\(－_＋╰\).html\"\> > \(╯－_－\)╯　┬——┬　　　　　　　　　╰\(－_－╰\).html
+echo \<meta http-equiv=\"Content-Type\" content=\"text/html\; charset=UTF-8\"\>\<meta http-equiv=\"refresh\" content=\"0.5\;url=\(╯－_－\)╯┬——┬　　　　　　　　　　╰\(－_－╰\).html\"\> > \(╯－_＋\)╯┴——┴　　　　　　　　　　╰\(－_＋╰\).html
